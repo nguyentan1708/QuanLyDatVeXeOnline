1f4152fa2e45a1adc00e9f594d7ccb8df61ed201
nguyentan1708 <nguyentan1708@gmail.com>
2022-02-22 15:57:08 +0700

    Start project

---
+++ src/main/java/com/grouptwo/QuanLyDatVeXeOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.grouptwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanLyDatVeXeOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanLyDatVeXeOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grouptwo/QuanLyDatVeXeOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grouptwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanLyDatVeXeOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
